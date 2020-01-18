7e3d813f01fd4ff5e031d23cca4476123bc44598
Evyatar <evyatar@tikalk.com>
2020-01-18 11:37:48 +0200

    stuff

---
+++ semanticui/navbar/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('.computer.only .dropdown.item')
+      .popup({
+        inline     : true,
+        hoverable  : true,
+        position   : 'bottom left',
+        delay: {
+          show: 300,
+          hide: 800
+        }
+      })
+    ;
+    $('.ui.dropdown').dropdown();
+    $('.ui.accordion').accordion();
+  
+    // bind "hide and show vertical menu" event to top right icon button 
+    $('.ui.toggle.button').click(function() {
+      $('.ui.vertical.menu').toggle("250", "linear")
+    });
+  });
