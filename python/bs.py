cfb42f406e2878368f1db707a2d26234c3f95165
Evyatar <evyatar@tikalk.com>
2020-03-15 00:17:14 +0200

    use thread pools

--- python/bs.py
+++ python/bs.py
@@ -5,6 +5,9 @@ import glob
 import logging
 from time import time,strptime
 from dateutil import parser
+from queue import Queue
+from threading import Thread
+
 
 DELTA = 2   # in days. if article date is less than DELTA days ago, it will be added to index
 LIMIT = 500
@@ -14,6 +17,58 @@ base_dir = '/home/evyatar/GitHub/github-pages-hello-world/haaretz/'
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
+
+def start_queue_with_workers(number_of_workers, f):
+    queue = Queue()
+    # Create worker threads
+    for x in range(number_of_workers):
+        worker = f(queue)
+        #worker = DownloadWorker(queue)
+        # Setting daemon to True will let the main thread exit even though the workers are blocking
+        worker.daemon = True
+        worker.start()
+    return queue
+
+class ArticleWorker(Thread):
+
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            # Get the work from the queue and expand the tuple
+            article = self.queue.get()
+            try:
+                do_with_article(article)
+            finally:
+                self.queue.task_done()
+
+articles_queue = start_queue_with_workers(8, lambda x: ArticleWorker(x))
+
+def send_article_to_queue(article):
+    articles_queue.put(article)
+
+
+class DownloadWorker(Thread):
+
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            # Get the work from the queue and expand the tuple
+            id, url = self.queue.get()
+            try:
+                article = readAndProcess(id, url)
+                send_article_to_queue(article)
+            finally:
+                self.queue.task_done()
+
+
+
+
 
 class Article:
     def __init__(self, id, header, publishedAt, updatedAt, fullHtml, subject, sub_subject):
@@ -36,26 +91,34 @@ def omit(tag):
     omitted = BeautifulSoup("<div>omitted " + tag + "</div>", "html.parser")
     return omitted
 
+def load_html(request):
+    response = urlopen(request)
+    html = response.read()
+    return html
+
 
 def readAndProcess(id, url):
     ts = time()
     logger.info("loading %s...", url)
     user_agent = 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
     request = Request(url, headers={'User-Agent': user_agent})
-    response = urlopen(request)
-    html = response.read()
-    logging.info('loading completed in %s seconds', time() - ts)
+    ##
+    #
+    html = load_html(request)
+    #
+    ##
+    logger.info('[%s] loading completed in %s seconds', id, time() - ts)
     ts = time()
-    logger.info("souping...")
+    logger.info("[%s] souping...", id)
     bs = BeautifulSoup(html, 'html.parser')
     try:
         header = bs.article.header.h1.contents[-1]
     except:
         header = "@@@"
 
-    logging.info('souping completed in %s seconds', time() - ts)
+    logger.info('[%s] souping completed in %s seconds', id, time() - ts)
     ts = time()
-    logger.info("processing...")
+    logger.info("[%s] processing...", id)
     sections = bs.article.findAll(name='section', class_='b-entry')
     if len(sections) == 0:
         return Article(id, header, '', '', '', '', '')
@@ -131,7 +194,7 @@ def readAndProcess(id, url):
     # assuming that 3rd child of <head> is not needed and can be changed...
     bs.html.head.contents[3].attrs={"name":"viewport","content":"width=device-width, initial-scale=1"}
     htmlText=bs.html.prettify()
-    logging.info('processing completed in %s seconds', time() - ts)
+    logger.info('[%s] processing completed in %s seconds', id, time() - ts)
     logger.debug("!")
     return Article(id, header, publishedAt, updatedAt, htmlText, subject, sub_subject)
 
@@ -145,14 +208,14 @@ article_ids = []
 #urls = [ 'https://www.haaretz.co.il/amp/1.8600567',
 
 
-def saveToFile(id, counter, htmlText):
+def saveToFile(id, htmlText):
     name = "h" + id + ".html"
     fileName = base_dir + name
     f = open(fileName, "w")
     f.write(htmlText)
     f.close()
 
-    logger.info("file: %s", fileName)
+    logger.info("[%s] file: %s", id, fileName)
     return name, fileName
 
 
@@ -163,6 +226,55 @@ def generate_index(articles):
         body = body + articleObject.link
     return construct_html(body, "")
 
+
+
+def send_urls_to_queue(ids):
+    logger.info("starting queue 1")
+    queue = start_queue_with_workers(8, lambda x: DownloadWorker(x))
+    for id in ids:
+        url = 'https://www.haaretz.co.il/amp/' + id
+        logger.info("[%s] inserting id %s in queue 1", id, id)
+        queue.put((id, url))    # will cause calling readAndProcess(id, url)
+    logger.info("completed inserting all IDs to queue")
+    queue.join()
+    logger.info("queue 1 joined")
+
+def do_with_article(articleObject):
+    try:
+        today = date.today()
+        #url = 'https://www.haaretz.co.il/amp/' + id
+        #articleObject = readAndProcess(id, url)
+        #so_far = so_far + 1
+        #logger.info("completed %d out of %d", so_far, numberOfIds)
+        if articleObject.fullHtml == '':
+            return
+        file_relative_path, file_full_path = saveToFile(articleObject.id, articleObject.fullHtml)
+        time = articleObject.updatedAt
+        if (time == ''):
+            time = articleObject.publishedAt
+        articleObject.link = '<p>[' + articleObject.subject + '/' + articleObject.sub_subject + ']<b>' + articleObject.publishedAt + '</b><a href="' + file_relative_path + '">' + str(
+            articleObject.header) + '</a></p>'
+        #body = body + articleObject.link
+        logger.info("[%s] published at %s, updated at: %s", articleObject.id, articleObject.publishedAt, articleObject.updatedAt)
+
+        # delta = today.day - parser.parse(articleObject.publishedAt).day
+        if (articleObject.publishedAt.startswith('2020-') and
+                today.day - parser.parse(articleObject.publishedAt).day < DELTA):
+            key = generate_key(articleObject)
+            add_article(key, articleObject)      #articles[key] = articleObject
+            logger.info("[%s] article added to index of today", articleObject.id)
+            #counter = counter + 1
+            #if counter > LIMIT:
+            #    break
+    except:
+        logger.error("some exception with id %s", id)
+
+articles = {}
+def add_article(key, article):
+    articles[key] = article
+    logger.info("[%s] article added to index of today with key %s", article.id, key)
+
+
 def doSomeIds(ids):
     articles = {}
     counter = 0
@@ -170,6 +282,7 @@ def doSomeIds(ids):
     body = ''
     today = date.today()
     numberOfIds = len(ids)
+    send_urls_to_queue(ids)
     for id in ids:
         try:
             url = 'https://www.haaretz.co.il/amp/' + id
@@ -185,8 +298,6 @@ def doSomeIds(ids):
             articleObject.link = '<p>['+articleObject.subject+'/'+articleObject.sub_subject +']<b>' + articleObject.publishedAt + '</b><a href="' + file_relative_path + '">' + str(articleObject.header) + '</a></p>'
             body = body + articleObject.link
             logger.info("published at %s, updated at: %s", articleObject.publishedAt, articleObject.updatedAt)
-
-            # delta = today.day - parser.parse(articleObject.publishedAt).day
             if (articleObject.publishedAt.startswith('2020-') and
                     today.day - parser.parse(articleObject.publishedAt).day < DELTA):
                 key = generate_key(articleObject)
@@ -197,14 +308,16 @@ def doSomeIds(ids):
                     break
         except:
             logger.error("some exception with id %s", id)
+    when_all_articles_added()
 
-    logger.info('generating index for %d articles...',len(articles))
+
+def when_all_articles_added():
+    logger.info('generating index for %d articles...', len(articles))
     html = generate_index(articles)
-    # html = '<html dir="rtl" lang="he"><head><meta charset="utf-8"/><meta content="width=device-width, initial-scale=1" name="viewport"/></head><body>' + \
-    #        body + existing_body + '</body></html>'
-    f = open(base_dir +'index.html', "w")
+    f = open(base_dir + 'index.html', "w")
     f.write(html)
     f.close()
+
 
 def generate_key(articleObject):
     return articleObject.subject + articleObject.sub_subject + articleObject.publishedAt + articleObject.id
@@ -331,11 +444,14 @@ def urls():
 
 if __name__ == "__main__":
     article_ids = first_page()
-    #print(len(article_ids))
-
     for url in urls():
         more_article_ids = process_page(url)
         article_ids.extend(more_article_ids)
         article_ids = remove_duplicates(article_ids)
-    logger.info("now reading %d articles",len(article_ids))
-    doSomeIds(article_ids)
+    logger.info("now sending %d articles",len(article_ids))
+    send_urls_to_queue(article_ids)
+    # at this point queue1 was joined (done inside send_urls_to_queue)
+
+    articles_queue.join()   # this will wait for queue2 completing?
+    logger.info("articles queue joined")
+    when_all_articles_added()
